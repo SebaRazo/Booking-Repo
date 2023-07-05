b0c0b9eab87d5a233903cfe2f86b9e59cec47a88
seba-razo <sebarazo14@gmail.com>
2023-07-04 21:15:29 -0300

    Cambios

--- src/main/java/com/proyecto/Booking/configuration/WebConfig.java
+++ src/main/java/com/proyecto/Booking/configuration/WebConfig.java
@@ -8,8 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+    public void addCorsMappings(CorsRegistry registry) {registry.addMapping("/**");}
 
 }
--- src/main/java/com/proyecto/Booking/controller/BookController.java
+++ src/main/java/com/proyecto/Booking/controller/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("book")
+@RequestMapping("book")/* cambio la barra?*/
 public class BookController {
 
     @Autowired
